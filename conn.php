57d7d6ecbd7110aad53ec7b50263200bc16f8e70
M-Ak-Nahari <146284729+M-Ak-Nahari@users.noreply.github.com>
2023-10-29 02:45:59 +0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "student");
-mysqli_select_db($conn, "student");
+$conn = mysqli_connect("sql12.freesqldatabase.com", "sql12657540", "pVAQJb91sW", "sql12657540");
+mysqli_select_db($conn, "sql12657540");
 ?>
